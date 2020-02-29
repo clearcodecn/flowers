e224258081e2c85b4a78c8d304b912e68fef96d3
mrj <git@clearcode.cn>
2020-02-29 20:13:35 +0800

    add password flag

--- cmd/client/cmd.go
+++ cmd/client/cmd.go
@@ -21,19 +21,21 @@ var (
 	argServerAddress string
 
 	argClientHTTPAddress string
+	argPassword          string
 )
 
 func init() {
 	Cmd.Flags().StringVarP(&argClientAddress, "addr", "a", "127.0.0.1:9011", "client listen address,if you want to listen all address please use 0.0.0.0:port")
 	Cmd.Flags().StringVarP(&argServerAddress, "saddr", "s", "", "server listen grpc address")
 	Cmd.Flags().StringVarP(&argClientHTTPAddress, "haddr", "", "127.0.0.1:8011", "client http address")
+	Cmd.Flags().StringVarP(&argServerAddress, "password", "p", "helloworld", "server password")
 }
 
 func run(cmd *cobra.Command, args []string) error {
 	client, err := server.NewClientProxyServer(
 		server.WithClientProxyAddress(argClientAddress),
 		server.WithServerProxyAddress(argServerAddress),
-		server.WithCipher("123456"),
+		server.WithCipher(argPassword),
 	)
 	if err != nil {
 		return err
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,16 +18,18 @@ var (
 	}
 
 	argServerAddress string
+	argPassword      string
 )
 
 func init() {
 	Cmd.Flags().StringVarP(&argServerAddress, "saddr", "s", ":9012", "server listen grpc address")
+	Cmd.Flags().StringVarP(&argServerAddress, "password", "p", "helloworld", "server password")
 }
 
 func run(cmd *cobra.Command, args []string) error {
 	s := server.NewProxyServer(
 		server.WithServerProxyAddress(argServerAddress),
-		server.WithCipher("123456"),
+		server.WithCipher(argPassword),
 	)
 	sig.RegisterClose(func() {
 		if err := s.Stop(); err != nil {
